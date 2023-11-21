2a2dde24062ee454a297567d9e9f2f51c4b6ee4e
Oussama Dalaa <62236767+Cosmos-0@users.noreply.github.com>
2023-11-21 19:53:41 +0100

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/convert', {
+      const response = await fetch('https://image-converter-plum.vercel.app/convert', {
         method: 'POST',
         body: formData,
       });
